495e0e8bd9f60b8148e453ba72233d86238d1207
jackfaller <85134985+jackfaller@users.noreply.github.com>
2023-11-21 14:46:14 -0500

    updated file

--- EEGstrument.py
+++ EEGstrument.py
@@ -8,10 +8,32 @@ Created on Wed Nov  8 15:45:49 2023
 
 # *****NOTE THE NAME MUST CHANGE!*****
 
-#set git
 
-#import UnicornPy
-import neurolThesis
+# #import UnicornPy
+# import neurolThesis
+# from neurolThesis import streams
+# from neurolThesis.connect_device import get_lsl_EEG_inlets
+# from neurolThesis.BCI import generic_BCI, automl_BCI
+# from neurolThesis import BCI_tools
+# from neurolThesis.models import classification_tools
+# from sys import exit
+# from pylsl import StreamInlet, resolve_stream
+
+# def clf(clf_input, clb_info):
+
+#     #clf_input = clf_input[:clb_info.shape[0]]
+
+#     note = 0
+#     for i in range(clb_info.shape[0]):
+#         if(clf_input > clb_info[i]):
+#             note += 1
+ 
+#     return note
+
+# #git test
+
+
+
 import tkinter as tk
 
 import random
@@ -34,6 +56,14 @@ def generate_letter():
         selected_letter = random.choice(letters)
         raise_box(selected_letter)
     root.after(2000, generate_letter)
+
+# def generate_letter(note):
+#     if all(position == 'normal' for position in box_positions.values()):
+#         letters = list(boxes.keys())
+#         selected_letter = letters[note]
+#         raise_box(selected_letter)
+#     #root.after(2000, generate_letter)
+
 
 def move_box():
     if current_highlighted:
@@ -97,10 +127,33 @@ window_width = width * len(colors)
 window_height = height + 100  # Extra space for buttons
 root.geometry(f"{window_width}x{window_height}")
 
+
+
+# streams1 = resolve_stream("name='Unicorn'")
+# inlet = StreamInlet(streams1[0])
+# stream = streams.lsl_stream(inlet, buffer_length=1024)
+
+# clb = lambda stream:  BCI_tools.band_power_calibrator(stream, ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 
+#                                                                'EEG 5', 'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', 
+#                                                         bands=['alpha_low','alpha_high'],
+#                                                         percentile=5, recording_length=10, epoch_len=1, inter_window_interval=0.25)
+
+
+# gen_tfrm = lambda buffer, clb_info: BCI_tools.band_power_transformer(buffer, clb_info, 
+#                                                                      ['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 
+#                                                                       'EEG 6', 'EEG 7', 'EEG 8'], 'unicorn', 
+#                                                         bands=['alpha_low','alpha_high'],
+#                                                         epoch_len=1)
+
+
+# BCI = generic_BCI(clf, transformer=gen_tfrm, action=generate_letter, calibrator=clb)
+# BCI.calibrate(stream)
+# BCI.run(stream)
+
+
 # Initialize the first call to start the process
 generate_letter()
 
 root.mainloop()
 
 
-#git test
